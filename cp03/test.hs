e8e2bc3041b71390395cfa4179a8db039fb20162
liuii <liuii@scse.hebut.edu.cn>
2017-03-06 22:50:50 +0800

    0306

---
+++ cp03/test.hs
@@ -0,0 +1,11 @@
+zeroto :: Int -> [Int]
+zeroto n = [0..n]
+
+add :: (Int, Int) -> Int
+add (x, y) = x + y
+
+add' :: Int -> (Int -> Int)
+add' x y = x + y
+
+mult :: Int -> (Int -> (Int -> Int))
+mult x y z = x * y * z
